4342a6f3e1353bd46a3fd9cce408ffa356917010
Sakib0017 <sakibkamal0017@gmail.com>
2024-04-24 15:36:33 +0600

    Updated side menus design

--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Tooltip } from "@material-tailwind/react";
 import "./tabs.css";
-
 const Sidemenu = () => {
   const [isVisible, setIsVisible] = useState(true);
 
@@ -17,110 +15,95 @@ const Sidemenu = () => {
   }, []);
   return (
     <>
-      <div>
-        <div className="hidden md:block">
-          <div>
-            <Link to="/branch" target="_blank" rel="noopener noreferrer">
-              <Tooltip
-                className="bg-gray-900/50  p-3  hover:cursor-pointer font-extrabold rounded-none"
-                content="Hotline"
-                placement="left"
-              >
-                <a
-                  target="_blank"
-                  className={`fixed touch-pinch-zoom top-[220px]  right-0   bg-emerald-900/80 shadow-md ${
-                    isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  <hr class="border-[1px]  w-20 border-[#1b7ced]" />
-                </a>
-              </Tooltip>
-            </Link>
-
-            <Link to="/sample" target="_blank" rel="noopener noreferrer">
-              <Tooltip
-                className="bg-gray-900/50  p-3 hover:cursor-pointer font-extrabold rounded-none"
-                content="Sample Collection"
-                placement="left"
-              >
-                <a
-                  target="_blank"
-                  className={`fixed top-[270px]  right-0 bg-emerald-900/80 shadow-md ${
-                    isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  <hr class="border-[1px]  w-20 border-[#1b7ced]" />
-                </a>
-              </Tooltip>
-            </Link>
-
-            <Tooltip
-              className="bg-gray-900/50  p-3 hover:cursor-pointer font-extrabold rounded-none"
-              content="Video Consultancy"
-              placement="left"
-            >
+      <div className="hidden fixed md:block">
+        <div>
+          <Link to="/branch" target="_blank" rel="noopener noreferrer">
+            <div className="bg-gray-900/50 fixed p-3  hover:cursor-pointer font-extrabold rounded-none">
+              <p className="fixed   rounded  p-2 top-[350px] flex right-[50px]   bg-[#00664a] shadow-md">
+                Hotline
+              </p>
               <a
                 target="_blank"
-                className={`fixed top-[320px] right-0 bg-emerald-900/80  shadow-md ${
+                className={`fixed touch-pinch-zoom top-[370px] flex right-0   bg-emerald-900/80 shadow-md ${
                   isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
                 }`}
               >
-                <hr class="border-[1px]  w-20 border-[#1b7ced]" />
+                <div className="w-[50px] h-1 bg-[#00664a]" />
               </a>
-            </Tooltip>
-
-            <Link to="/patient" target="_blank" rel="noopener noreferrer">
-              <Tooltip
-                className="bg-gray-900/50  p-3 hover:cursor-pointer font-extrabold rounded-none"
-                content="Report Download"
-                placement="left"
-              >
-                <a
-                  target="_blank"
-                  className={`fixed top-[370px]   right-0 bg-emerald-900/80 shadow-md ${
-                    isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  <hr class="border-[1px]  w-20 border-[#1b7ced]" />
-                </a>
-              </Tooltip>
-            </Link>
-
-            <Link to="/complain" target="_blank" rel="noopener noreferrer">
-              <Tooltip
-                className="bg-gray-900/50  p-3 hover:cursor-pointer font-extrabold rounded-none"
-                content="Complain Submission"
-                placement="left"
-              >
-                <a
-                  target="_blank"
-                  className={`fixed top-[420px]   right-0 bg-emerald-900/80 shadow-md ${
-                    isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  <hr class="border-[1px]  w-20 border-[#1b7ced]" />
-                </a>
-              </Tooltip>
-            </Link>
-
-            <Link>
-              {" "}
-              <div
-                className="bg-gray-900/50  p-3 hover:cursor-pointer font-extrabold rounded-none"
-                content="Messanger"
-                placement="left"
-              >
-                <a
-                  target="_blank"
-                  className={`fixed top-[470px]      right-0 bg-emerald-900/80 shadow-md ${
-                    isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  <hr class="border-[1px]  w-20 border-[#1b7ced]" />
-                </a>
-              </div>
-            </Link>
+            </div>
+          </Link>
+          <div className="bg-gray-900/50 fixed p-3 hover:cursor-pointer font-extrabold rounded-none">
+            <p className="fixed  rounded touch-pinch-zoom p-2 top-[400px] flex right-[50px]   bg-[#00664a] shadow-md">
+              Messanger
+            </p>
+            <a
+              className={`fixed touch-pinch-zoom top-[420px] flex right-0   bg-emerald-900/80 shadow-md ${
+                isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+              }`}
+            >
+              <div className="w-[50px] h-1 bg-[#00664a]" />
+            </a>
           </div>
+          <Link to="/report" target="_blank" rel="noopener noreferrer">
+            <div className="bg-gray-900/50 fixed p-3 hover:cursor-pointer font-extrabold rounded-none">
+              <p className="fixed rounded touch-pinch-zoom p-2 top-[450px] flex right-[50px]   bg-[#00664a] shadow-md">
+                Report Download
+              </p>
+              <a
+                target="_blank"
+                className={`fixed touch-pinch-zoom top-[470px] flex right-0   bg-emerald-900/80 shadow-md ${
+                  isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+              >
+                <div className="w-[50px] h-1 bg-[#00664a]" />
+              </a>
+            </div>
+          </Link>
+          <Link to="/sample" target="_blank" rel="noopener noreferrer">
+            <div className="bg-gray-900/50 fixed p-3 hover:cursor-pointer font-extrabold rounded-none">
+              <p className="fixed rounded touch-pinch-zoom p-2 top-[500px] flex right-[50px]   bg-[#00664a] shadow-md">
+                Sample Collection
+              </p>
+              <a
+                target="_blank"
+                className={`fixed touch-pinch-zoom top-[520px] flex right-0   bg-emerald-900/80 shadow-md ${
+                  isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+              >
+                <div className="w-[50px] h-1 bg-[#00664a]" />
+              </a>
+            </div>
+          </Link>
+
+          <div className="bg-gray-900/50 fixed p-3 hover:cursor-pointer font-extrabold rounded-none">
+            <p className="fixed rounded touch-pinch-zoom p-2 top-[550px] flex right-[50px]   bg-[#00664a] shadow-md">
+              Video Consutancy
+            </p>
+            <a
+              target="_blank"
+              className={`fixed touch-pinch-zoom top-[570px] flex right-0   bg-emerald-900/80 shadow-md ${
+                isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+              }`}
+            >
+              <div className="w-[50px] h-1 bg-[#00664a]" />
+            </a>
+          </div>
+
+          <Link to="/complain" target="_blank" rel="noopener noreferrer">
+            <div className="bg-gray-900/50 fixed p-3 hover:cursor-pointer font-extrabold rounded-none">
+              <p className="fixed rounded touch-pinch-zoom p-2 top-[600px] flex right-[50px]   bg-[#00664a] shadow-md">
+                Complain Submition
+              </p>
+              <a
+                target="_blank"
+                className={`fixed touch-pinch-zoom top-[620px] flex right-0   bg-emerald-900/80 shadow-md ${
+                  isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+              >
+                <div className="w-[50px] h-1 bg-[#00664a]" />
+              </a>
+            </div>
+          </Link>
         </div>
       </div>
     </>
