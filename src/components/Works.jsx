7675689595643923714ccc82a0b85b7988d4882a
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-04-30 13:00:33 +0600

    Allignment fixed, refactored code to eleminate redundent css

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -107,100 +107,96 @@ const Works = () => {
       <div className="relative pt-10 fontFamily-ubuntu">
         <div className=" overflow-hidden  mt-[-140px] py-24 sm:py-32">
           <div className="mx-auto">
-            <div className="mx-auto flex justify-evenly rounded-3xl bg-gradient-to-b from-transparent to-[#f0fff0]/60 py-5 px-20">
-              <div className="lg:pr-8 lg:pt-4">
-                <div className="grid md:grid-cols-4 gap-5 items-center justify-center mx-auto ">
-                  <div className="flex flex-col col-span-1 items-center  mx-auto group">
-                    <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
-                      <div className=" rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
-                        <svg
-                          className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
-                          viewBox="0 0 640 512"
-                          xmlns="http://www.w3.org/2000/svg">
-                          {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-                          <path d="M192 48c0-26.5 21.5-48 48-48H400c26.5 0 48 21.5 48 48V512H368V432c0-26.5-21.5-48-48-48s-48 21.5-48 48v80H192V48zM48 96H160V512H48c-26.5 0-48-21.5-48-48V320H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V224H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V144c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v48H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80v64H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80V464c0 26.5-21.5 48-48 48H480V96H592zM312 64c-8.8 0-16 7.2-16 16v24H272c-8.8 0-16 7.2-16 16v16c0 8.8 7.2 16 16 16h24v24c0 8.8 7.2 16 16 16h16c8.8 0 16-7.2 16-16V152h24c8.8 0 16-7.2 16-16V120c0-8.8-7.2-16-16-16H344V80c0-8.8-7.2-16-16-16H312z"></path>
-                        </svg>
-                      </div>
-                      <div>
-                        <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
-                          <Number n={22} />
-                        </h6>
-                        <p className="text-gray-500 text-center font-bold font-ubuntu text-[16px]">
-                          BRANCHES
-                        </p>
-                      </div>
+            <div className="flex flex-wrap justify-center items-center bg-gradient-to-b from-transparent to-[#f0fff0]/60 py-5">
+              <div className="flex flex-col lg:flex-row gap-5 mx-auto ">
+                <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center group">
+                  <div className="rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
+                    <svg
+                      className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
+                      viewBox="0 0 640 512"
+                      xmlns="http://www.w3.org/2000/svg">
+                      {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+                      <path d="M192 48c0-26.5 21.5-48 48-48H400c26.5 0 48 21.5 48 48V512H368V432c0-26.5-21.5-48-48-48s-48 21.5-48 48v80H192V48zM48 96H160V512H48c-26.5 0-48-21.5-48-48V320H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V224H80c8.8 0 16-7.2 16-16s-7.2-16-16-16H0V144c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v48H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80v64H560c-8.8 0-16 7.2-16 16s7.2 16 16 16h80V464c0 26.5-21.5 48-48 48H480V96H592zM312 64c-8.8 0-16 7.2-16 16v24H272c-8.8 0-16 7.2-16 16v16c0 8.8 7.2 16 16 16h24v24c0 8.8 7.2 16 16 16h16c8.8 0 16-7.2 16-16V152h24c8.8 0 16-7.2 16-16V120c0-8.8-7.2-16-16-16H344V80c0-8.8-7.2-16-16-16H312z"></path>
+                    </svg>
+                  </div>
+                  <div>
+                    <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
+                      <Number n={22} />
+                    </h6>
+                    <p className="text-gray-500 text-center font-bold font-ubuntu text-[16px]">
+                      BRANCHES
+                    </p>
+                  </div>
+                </div>
+                <div className="flex mx-auto group">
+                  <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
+                    <div className="rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
+                      <svg
+                        className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
+                        viewBox="0 0 512 512"
+                        xmlns="http://www.w3.org/2000/svg">
+                        {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+                        <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96v32V480H384V128 96 56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM96 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H96V96zM416 480h32c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H416V480zM224 208c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v48h48c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H288v48c0 8.8-7.2 16-16 16H240c-8.8 0-16-7.2-16-16V320H176c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h48V208z"></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
+                        <div className="flex items-center justify-center">
+                          <Number n={7} />
+                          <span>k+</span>
+                        </div>
+                      </h6>
+                      <p className="text-gray-500 text-center font-bold font-ubuntu text-[16px]">
+                        SERVICES
+                      </p>
                     </div>
                   </div>
-                  <div className="flex flex-col col-span-1 items-center  mx-auto group">
-                    <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
-                      <div className=" rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
-                        <svg
-                          className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
-                          viewBox="0 0 512 512"
-                          xmlns="http://www.w3.org/2000/svg">
-                          {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-                          <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96v32V480H384V128 96 56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM96 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H96V96zM416 480h32c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H416V480zM224 208c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v48h48c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H288v48c0 8.8-7.2 16-16 16H240c-8.8 0-16-7.2-16-16V320H176c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h48V208z"></path>
-                        </svg>
-                      </div>
-                      <div>
-                        <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
-                          <div className="flex items-center justify-center">
-                            <Number n={7} />
-                            <span>k+</span>
-                          </div>
-                        </h6>
-                        <p className="text-gray-500 text-center font-bold font-ubuntu text-[16px]">
-                          SERVICES
-                        </p>
-                      </div>
+                </div>
+                <div className="flex mx-auto group">
+                  <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
+                    <div className="rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
+                      <svg
+                        className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
+                        viewBox="0 0 512 512"
+                        xmlns="http://www.w3.org/2000/svg">
+                        {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM164.1 325.5C182 346.2 212.6 368 256 368s74-21.8 91.9-42.5c5.8-6.7 15.9-7.4 22.6-1.6s7.4 15.9 1.6 22.6C349.8 372.1 311.1 400 256 400s-93.8-27.9-116.1-53.5c-5.8-6.7-5.1-16.8 1.6-22.6s16.8-5.1 22.6 1.6zm53.5-96.7l0 0 0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0zm160 0l0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0 0 0z"></path>
+                      </svg>
+                    </div>
+                    <div>
+                      <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
+                        <div className="flex items-center justify-center">
+                          <Number n={82} />
+                          <span>k+</span>
+                        </div>
+                      </h6>
+                      <p className="text-gray-500  font-bold font-ubuntu text-[16px]">
+                        HAPPY PATIENTS
+                      </p>
                     </div>
                   </div>
-                  <div className="flex flex-col col-span-1 items-center  mx-auto group">
-                    <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
-                      <div className=" rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
-                        <svg
-                          className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
-                          viewBox="0 0 512 512"
-                          xmlns="http://www.w3.org/2000/svg">
-                          {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-                          <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM164.1 325.5C182 346.2 212.6 368 256 368s74-21.8 91.9-42.5c5.8-6.7 15.9-7.4 22.6-1.6s7.4 15.9 1.6 22.6C349.8 372.1 311.1 400 256 400s-93.8-27.9-116.1-53.5c-5.8-6.7-5.1-16.8 1.6-22.6s16.8-5.1 22.6 1.6zm53.5-96.7l0 0 0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0zm160 0l0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0 0 0z"></path>
-                        </svg>
-                      </div>
-                      <div>
-                        <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
-                          <div className="flex items-center justify-center">
-                            <Number n={82} />
-                            <span>k+</span>
-                          </div>
-                        </h6>
-                        <p className="text-gray-500  font-bold font-ubuntu text-[16px]">
-                          HAPPY PATIENTS
-                        </p>
-                      </div>
+                </div>
+                <div className="flex mx-auto group">
+                  <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center">
+                    <div className="rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
+                      <svg
+                        className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
+                        viewBox="0 0 448 512"
+                        xmlns="http://www.w3.org/2000/svg">
+                        {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+                        <path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"></path>
+                      </svg>
                     </div>
-                  </div>
-                  <div className="flex flex-col col-span-1 items-center  mx-auto group">
-                    <div className="p-5 items-center flex flex-row gap-5 mx-auto text-center ">
-                      <div className=" rounded-full p-3 border-2 border-gray-600  group-hover:bg-gray-600">
-                        <svg
-                          className="w-[40px] h-[60px] fill-gray-600 group-hover:fill-white"
-                          viewBox="0 0 448 512"
-                          xmlns="http://www.w3.org/2000/svg">
-                          {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-                          <path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"></path>
-                        </svg>
-                      </div>
-                      <div>
-                        <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
-                          <div className="flex items-center justify-center">
-                            <Number n={28} />
-                            <span>+</span>
-                          </div>
-                        </h6>
-                        <p className="text-gray-500  font-bold font-ubuntu text-[16px]">
-                          YEARS EXPERIANCES
-                        </p>
-                      </div>
+                    <div>
+                      <h6 className="text-gray-600 text-center font-bold font-ubuntu text-[40px]">
+                        <div className="flex items-center justify-center">
+                          <Number n={28} />
+                          <span>+</span>
+                        </div>
+                      </h6>
+                      <p className="text-gray-500  font-bold font-ubuntu text-[16px]">
+                        YEARS EXPERIANCES
+                      </p>
                     </div>
                   </div>
                 </div>
