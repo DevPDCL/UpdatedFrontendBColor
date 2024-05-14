b6cd02f5e6024731ce6260ed04807e9fcd0ab435
Sakib0017 <sakibkamal0017@gmail.com>
2024-05-14 16:01:23 +0600

    marquee changes

--- src/components/Cor.jsx
+++ src/components/Cor.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { service, technologies } from "../constants";
-
+import Marquee from "react-fast-marquee";
 function Cor() {
   return (
     <div className="mt-[150px] -mb-[10px]">
       <h3 className="text-gray-900/50 text-center text-[28px] font-ubuntu">
         Corporate Partners
       </h3>
-
-      <div className="flex flex-row flex-wrap mt-10 sm:w-[80%] max-w-screen-xl mx-auto justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            {/* Set explicit width and height on the image element */}
-            <img
-              src={technology.icon}
-              alt={technology.name}
-              width="112"
-              height="112"
-            />
-          </div>
-        ))}
-      </div>
-
+        <div className="flex flex-row flex-wrap mt-10 sm:w-[80%] max-w-screen-xl mx-auto justify-center gap-10">
+           <Marquee>
+          {technologies.map((technology) => (
+            <div className="mx-5 w-28 h-28" key={technology.name}>
+              {/* Set explicit width and height on the image element */}
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                width="112"
+                height="112"
+              />
+            </div>
+          ))}
+          </Marquee>
+        </div>
       <h3 className="text-gray-900/50 text-center text-[28px] font-ubuntu">
         Service Partners
       </h3>
